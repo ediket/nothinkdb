51cfd9ada36e9b6d5f79f078b1e5d765d501a0d8
ironhee <leechulhee95@gmail.com>
2016-02-15 17:33:59 +0900

    create example - following.

---
+++ example/following.js
@@ -0,0 +1,58 @@
+/* eslint no-console: 0 */
+import r from 'rethinkdb';
+import { Table, belongsToMany } from '../';
+import Joi from 'joi';
+
+const userTable = new Table({
+  table: 'user',
+  schema: () => ({
+    ...Table.schema,
+    name: Joi.string().required(),
+  }),
+  relations: () => ({
+    follower: belongsToMany([userTable.linkedBy(followingTable, 'followerId'), followingTable.linkTo(userTable, 'followeeId')]),
+    followee: belongsToMany([userTable.linkedBy(followingTable, 'followeeId'), followingTable.linkTo(userTable, 'followerId')]),
+  }),
+});
+const followingTable = new Table({
+  table: 'following',
+  schema: () => ({
+    ...Table.schema,
+    followerId: userTable.getForeignKey({ isManyToMany: true }),
+    followeeId: userTable.getForeignKey({ isManyToMany: true }),
+  }),
+});
+
+(async () => {
+  const connection = await r.connect({
+    db: 'test',
+  });
+
+  await userTable.sync(connection);
+  await followingTable.sync(connection);
+  userTable.query().delete().run(connection);
+  followingTable.query().delete().run(connection);
+
+  async function loadUsersWithJoin() {
+    let userQuery = userTable.query().coerceTo('array');
+    userQuery = userTable.withJoin(userQuery, { follower: true, followee: true });
+    const users = await userQuery.run(connection);
+    return users;
+  }
+
+  const followee = userTable.create({ name: 'foo' });
+  const follower = userTable.create({ name: 'foo' });
+  await userTable.insert(followee).run(connection);
+  await userTable.insert(follower).run(connection);
+
+  await userTable.createRelation('follower', followee.id, follower.id).run(connection);
+  console.log(await loadUsersWithJoin());
+  await userTable.removeRelation('follower', followee.id, follower.id).run(connection);
+  console.log(await loadUsersWithJoin());
+
+  await connection.close();
+})()
+.catch(e => {
+  console.log(e);
+  process.exit();
+});
