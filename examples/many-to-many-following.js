08cba72d5a90300afd890a615dc18fedca707739
ironhee <leechulhee95@gmail.com>
2016-02-17 16:27:59 +0900

    follower -> followers, followee -> following

--- examples/many-to-many-following.js
+++ examples/many-to-many-following.js
@@ -10,8 +10,8 @@ const userTable = new Table({
     name: Joi.string().required(),
   }),
   relations: () => ({
-    follower: belongsToMany([userTable.linkedBy(followingTable, 'followerId'), followingTable.linkTo(userTable, 'followeeId')]),
-    followee: belongsToMany([userTable.linkedBy(followingTable, 'followeeId'), followingTable.linkTo(userTable, 'followerId')]),
+    followers: belongsToMany([userTable.linkedBy(followingTable, 'followerId'), followingTable.linkTo(userTable, 'followeeId')]),
+    following: belongsToMany([userTable.linkedBy(followingTable, 'followeeId'), followingTable.linkTo(userTable, 'followerId')]),
   }),
 });
 const followingTable = new Table({
@@ -35,20 +35,23 @@ async function run() {
 
   async function loadUsersWithJoin() {
     let userQuery = userTable.query().coerceTo('array');
-    userQuery = userTable.withJoin(userQuery, { follower: true, followee: true });
+    userQuery = userTable.withJoin(userQuery, { followers: true, following: true });
     const users = await userQuery.run(connection);
     return users;
   }
 
   const followee = userTable.create({ name: 'foo' });
-  const follower = userTable.create({ name: 'foo' });
+  const follower1 = userTable.create({ name: 'foo' });
+  const follower2 = userTable.create({ name: 'foo' });
   await userTable.insert(followee).run(connection);
-  await userTable.insert(follower).run(connection);
+  await userTable.insert(follower1).run(connection);
+  await userTable.insert(follower2).run(connection);
 
-  await userTable.createRelation('follower', followee.id, follower.id).run(connection);
+  await userTable.createRelation('followers', followee.id, follower1.id).run(connection);
+  await userTable.createRelation('followers', followee.id, follower2.id).run(connection);
   console.log('after create:');
   console.log(await loadUsersWithJoin());
-  await userTable.removeRelation('follower', followee.id, follower.id).run(connection);
+  await userTable.removeRelation('followers', followee.id, follower1.id).run(connection);
   console.log('after remove:');
   console.log(await loadUsersWithJoin());
 
